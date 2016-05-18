9836903a9715be0a1e9331c38387f2f98c17151a
Steve <sonvq@qsoftvietnam.com>
2016-05-18 15:51:47 +0700

    commit

--- facebook.php
+++ facebook.php
@@ -1,23 +1,36 @@
 <?php
-
-$homepage = file_get_contents('https://graph.facebook.com/me/?fields=id,name,first_name,last_name,email&access_token=EAACEdEose0cBAAR4f3eb7YmyjqAL3rqEd9J86nCtZCRuU1cNFRLTPrhMQQUfs4u07MCXjKl3vjcSoV7erdRhFGbZCYpP7V1la8iGZBZBqFivO2MXBUtJKwFnyxqP9y1vlZBMNBoRAZCZCAR2gJlAf0I2qe2oIPYjYEwYvKlKyif3gZDZD');
-echo $homepage;
-
-function get_remote_data($url, $post_paramtrs = false) {
+echo '<b>readfile: </b><br/>';
+echo readfile("https://graph.facebook.com/me/?fields=id,name,first_name,last_name,email&access_token=EAACEdEose0cBAAR4f3eb7YmyjqAL3rqEd9J86nCtZCRuU1cNFRLTPrhMQQUfs4u07MCXjKl3vjcSoV7erdRhFGbZCYpP7V1la8iGZBZBqFivO2MXBUtJKwFnyxqP9y1vlZBMNBoRAZCZCAR2gJlAf0I2qe2oIPYjYEwYvKlKyif3gZDZD");   //needs "Allow_url_include" enabled
+echo '<br/>';
+//OR
+//echo include("https://graph.facebook.com/me/?fields=id,name,first_name,last_name,email&access_token=EAACEdEose0cBAAR4f3eb7YmyjqAL3rqEd9J86nCtZCRuU1cNFRLTPrhMQQUfs4u07MCXjKl3vjcSoV7erdRhFGbZCYpP7V1la8iGZBZBqFivO2MXBUtJKwFnyxqP9y1vlZBMNBoRAZCZCAR2gJlAf0I2qe2oIPYjYEwYvKlKyif3gZDZD");    //needs "Allow_url_include" enabled
+//OR
+echo '<b>file_get_contents: </b><br/>';
+echo file_get_contents("https://graph.facebook.com/me/?fields=id,name,first_name,last_name,email&access_token=EAACEdEose0cBAAR4f3eb7YmyjqAL3rqEd9J86nCtZCRuU1cNFRLTPrhMQQUfs4u07MCXjKl3vjcSoV7erdRhFGbZCYpP7V1la8iGZBZBqFivO2MXBUtJKwFnyxqP9y1vlZBMNBoRAZCZCAR2gJlAf0I2qe2oIPYjYEwYvKlKyif3gZDZD");
+echo '<br/>';
+//OR
+echo '<b>stream_get_contents: </b><br/>';
+echo stream_get_contents(fopen('https://graph.facebook.com/me/?fields=id,name,first_name,last_name,email&access_token=EAACEdEose0cBAAR4f3eb7YmyjqAL3rqEd9J86nCtZCRuU1cNFRLTPrhMQQUfs4u07MCXjKl3vjcSoV7erdRhFGbZCYpP7V1la8iGZBZBqFivO2MXBUtJKwFnyxqP9y1vlZBMNBoRAZCZCAR2gJlAf0I2qe2oIPYjYEwYvKlKyif3gZDZD', "rb")); //you may use "r" instead of "rb"  //needs "Allow_url_fopen" enabled
+echo '<br/>';
+	
+function get_remote_data($url, $post_paramtrs=false)
+{
     $c = curl_init();
     curl_setopt($c, CURLOPT_URL, $url);
     curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-    if ($post_paramtrs) {
-        curl_setopt($c, CURLOPT_POST, TRUE);
-        curl_setopt($c, CURLOPT_POSTFIELDS, "var1=bla&" . $post_paramtrs);
+    if($post_paramtrs)
+    {
+        curl_setopt($c, CURLOPT_POST,TRUE);
+        curl_setopt($c, CURLOPT_POSTFIELDS, "var1=bla&".$post_paramtrs );
     }
-    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($c, CURLOPT_SSL_VERIFYHOST,false);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER,false);
     curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0");
     curl_setopt($c, CURLOPT_COOKIE, 'CookieName1=Value;');
     curl_setopt($c, CURLOPT_MAXREDIRS, 10);
-    $follow_allowed = ( ini_get('open_basedir') || ini_get('safe_mode')) ? false : true;
-    if ($follow_allowed) {
+    $follow_allowed= ( ini_get('open_basedir') || ini_get('safe_mode')) ? false:true;
+    if ($follow_allowed)
+    {
         curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
     }
     curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);
@@ -25,32 +38,41 @@ function get_remote_data($url, $post_paramtrs = false) {
     curl_setopt($c, CURLOPT_TIMEOUT, 60);
     curl_setopt($c, CURLOPT_AUTOREFERER, true);
     curl_setopt($c, CURLOPT_ENCODING, 'gzip,deflate');
-    $data = curl_exec($c);
-    $status = curl_getinfo($c);
+    $data=curl_exec($c);
+    $status=curl_getinfo($c);
     curl_close($c);
-    preg_match('/(http(|s)):\/\/(.*?)\/(.*\/|)/si', $status['url'], $link);
-    $data = preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/|\/)).*?)(\'|\")/si', '$1=$2' . $link[0] . '$3$4$5', $data);
-    $data = preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/)).*?)(\'|\")/si', '$1=$2' . $link[1] . '://' . $link[3] . '$3$4$5', $data);
-    if ($status['http_code'] == 200) {
+    preg_match('/(http(|s)):\/\/(.*?)\/(.*\/|)/si',  $status['url'],$link); $data=preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/|\/)).*?)(\'|\")/si','$1=$2'.$link[0].'$3$4$5', $data);   $data=preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/)).*?)(\'|\")/si','$1=$2'.$link[1].'://'.$link[3].'$3$4$5', $data);
+    if($status['http_code']==200)
+    {
         return $data;
-    } elseif ($status['http_code'] == 301 || $status['http_code'] == 302) {
-        if (!$follow_allowed) {
-            if (!empty($status['redirect_url'])) {
-                $redirURL = $status['redirect_url'];
-            } else {
-                preg_match('/href\=\"(.*?)\"/si', $data, $m);
-                if (!empty($m[1])) {
-                    $redirURL = $m[1];
+    }
+    elseif($status['http_code']==301 || $status['http_code']==302)
+    {
+        if (!$follow_allowed)
+        {
+            if (!empty($status['redirect_url']))
+            {
+                $redirURL=$status['redirect_url'];
+            }
+            else
+            {
+                preg_match('/href\=\"(.*?)\"/si',$data,$m);
+                if (!empty($m[1]))
+                {
+                    $redirURL=$m[1];
                 }
             }
-            if (!empty($redirURL)) {
-                return call_user_func(__FUNCTION__, $redirURL, $post_paramtrs);
+            if(!empty($redirURL))
+            {
+                return  call_user_func( __FUNCTION__, $redirURL, $post_paramtrs);
             }
         }
     }
-    return "ERRORCODE22 with $url!!<br/>Last status codes<b/>:" . json_encode($status) . "<br/><br/>Last data got<br/>:$data";
+    return "ERRORCODE22 with $url!!<br/>Last status codes<b/>:".json_encode($status)."<br/><br/>Last data got<br/>:$data";
 }
 
+echo '<b>curl: </b><br/>';
 $a = get_remote_data('https://graph.facebook.com/me/?fields=id,name,first_name,last_name,email&access_token=EAACEdEose0cBAAR4f3eb7YmyjqAL3rqEd9J86nCtZCRuU1cNFRLTPrhMQQUfs4u07MCXjKl3vjcSoV7erdRhFGbZCYpP7V1la8iGZBZBqFivO2MXBUtJKwFnyxqP9y1vlZBMNBoRAZCZCAR2gJlAf0I2qe2oIPYjYEwYvKlKyif3gZDZD');
 echo($a);
+echo '<br/>';
 ?>
